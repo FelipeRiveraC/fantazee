939e5c2e17e4206fc34f4c54945424ae50d7073a
Lucas Palacios <lpalacios1@uc.cl>
2025-02-14 12:31:39 -0500

    Remove unnecessary code in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-Engagement.find_or_create_by(name: 'Canned Project')
-Engagement.find_or_create_by(name: 'Lean Startup')
-
 if Rails.env.development?
   AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
